d03a445cf487c3394fcdf9f5ab7dfb26036fd2de
ShaneD <43129046+SDzartov@users.noreply.github.com>
2023-05-21 19:23:28 +1000

    Added Files

---
+++ BotFormRecognizer/FormAnalysis/DocumentExtract.cs
@@ -0,0 +1,43 @@
+﻿using Azure.AI.FormRecognizer.DocumentAnalysis;
+using Azure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents.SystemFunctions;
+
+namespace BotFormRecognizer.FormAnalysis
+{
+
+    internal class DocumentExtract
+    {
+        readonly DocumentAnalysisClient client;
+        public DocumentExtract(string key, string endpoint)
+        {
+            var credential = new AzureKeyCredential(key);
+            client = new DocumentAnalysisClient(new Uri(endpoint), credential);
+        }
+
+        public async Task<string> Extract(string url)
+        {
+            var documentUrl = new Uri(url);
+            var operation = await client.AnalyzeDocumentFromUriAsync(WaitUntil.Completed, "model01", documentUrl);
+            var cards = operation.Value;
+            return ExtractValue(cards);
+        }
+
+        private string ExtractValue(AnalyzeResult businessCards)
+        {
+            var contactName = string.Empty;
+
+            businessCards.Documents[0].Fields.TryGetValue("graphics", out var graphicsField);
+
+            if (graphicsField != null)
+            {
+                var graphicsValue = graphicsField.Value.AsString();
+                contactName = graphicsValue;
+            }
+        return contactName;
+        }    
+    }
+}
---
+++ BotFormRecognizer/Settings.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Components.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotFormRecognizer
+{
+    internal static class Settings
+    {
+        public static string ConnectionString = "DefaultEndpointsProtocol=https;AccountName=deakin19073460464;AccountKey=j/Cx75OcHnHuAhRdXbPYsEJ+6VLskh2v3hRIrd4lEkRf/BtyD8c3ieY7eSh2YHD1Uz0sljat77B/+AStzkntwg==;EndpointSuffix=core.windows.net";
+        public static string ContainerName = "formbotstorage";
+        public static string Key = "j/Cx75OcHnHuAhRdXbPYsEJ+6VLskh2v3hRIrd4lEkRf/BtyD8c3ieY7eSh2YHD1Uz0sljat77B/+AStzkntwg==";
+        public static string Endpoint = "https://deakin19073460464.blob.core.windows.net/formbotstorage";
+
+        public static string FormKey = "e031556998fc4061bf83e09d732b487e";
+        public static string FormEndpoint = "https://formreconshane01.cognitiveservices.azure.com/";
+        
+    }
+}
